7a3ca1892f3e12eb6491a58324cc27f78b511f27
DESKTOP-O403M3T\user <ta>
2023-07-27 21:34:44 +0900

    feat : example sprint jpa

--- src/main/java/com/hong/spring/ex/jpa/JpaStudentController.java
+++ src/main/java/com/hong/spring/ex/jpa/JpaStudentController.java
@@ -1,11 +1,15 @@
 package com.hong.spring.ex.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hong.spring.ex.jpa.domain.Student;
+import com.hong.spring.ex.jpa.repository.StudentRepository;
 import com.hong.spring.ex.jpa.service.StudentService;
 
 @Controller
@@ -13,6 +17,7 @@ public class JpaStudentController {
 	
 	@Autowired
 	private StudentService studentService;
+	private StudentRepository studentRepository;
 	
 	@GetMapping("/lombok/test")
 	@ResponseBody
@@ -56,5 +61,26 @@ public class JpaStudentController {
 		studentService.deleteStudent(1);
 		
 		return "삭제성공";
+	}
+	
+	@GetMapping("/jpa/student/select")
+	public List<Student> selectFindAll() {
+		//List<Student> list = studentRepository.findTop1ByOrderByIdDesc();
+		
+		//전달받은 이름을 가진 모든 데이터 조회
+		List<String> nameList = new ArrayList<>();
+		nameList.add("홍지혁");
+		nameList.add("조세휴");
+		List<Student> list = studentRepository.findByNameIn(nameList);
+		return list;
+	}
+	
+	@GetMapping("/jpa/student/native")
+	@ResponseBody
+	public List<Student> nativeStudent(){
+		//장래희망이 변호사인 데이터 조회
+		List<Student> list = studentRepository.findByMajorNative("변호사");
+		
+		return list;
 	}
 }
--- src/main/java/com/hong/spring/ex/jpa/repository/StudentRepository.java
+++ src/main/java/com/hong/spring/ex/jpa/repository/StudentRepository.java
@@ -1,6 +1,10 @@
 package com.hong.spring.ex.jpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hong.spring.ex.jpa.domain.Student;
@@ -8,4 +12,34 @@ import com.hong.spring.ex.jpa.domain.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer>{
 	
+	//id를 기준으로 내림차순으로 정렬한 모든 행 데이터
+	public List<Student> findAllByOrderByIdDesc();
+	
+	//id를 기준으로 내림차순 정렬 후 하나만 조회
+	public List<Student> findTop1ByOrderByIdDesc();
+	
+	//name 컬럼이 일치하는 데이터 조회
+	public List<Student> findByName(String name);
+	
+	//전달받은 이름들과 일치하는 모든 데이터 조회
+	//name in ('홍지혁', '유제석', '조새휴')
+	public List<Student> findByNameIn(List<String> nameList);
+	
+	//major 컬럼에 특정 문자열이 포함된 데이터 조회
+	// major like "%naver%"
+	public List<Student> findByMajorContaining(String major);
+	
+	//이름이 특정 문자열로 시작되는 데이터 조회
+	//name like "홍%"
+	public List<Student> findByNameStartingWith(String name);
+	
+	
+	//id가 n에서 m사이에 대응되는 데이터 조회
+	//between n and m
+	public List<Student> findByIdBetweenOrderByIdDesc(int start, int end);
+	
+	//쿼리를 통해 특정 잘래희망을 가진 데이터 조회
+	//네이티브 쿼리를 수행하는 어노테이션
+	@Query(value="select * from `student` where `major` = :major", nativeQuery=true)
+	public List<Student> findByMajorNative(@Param("major") String major);
 }
